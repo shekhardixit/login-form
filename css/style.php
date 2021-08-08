26d9b1819e6497afaa6b99b8a99287fc995eb0f8
shekhardixit <500067690@stu.upes.ac.in>
2021-08-08 17:01:52 +0530

    Registration Form page created

---
+++ css/style.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+    <title></title>
+	<style type="text/css">
+	
+	*{
+		margin: 0;
+		padding: 0;
+		font-family: 'Raleway', sans-serif;
+		box-sizing: border-box;
+	}
+	.reg-section{
+		
+		width: 100%;
+		height: 100vh;
+		background-image: linear-gradient(27deg, #013A6B 50%, #004E95 50%);
+	}
+	.centerdiv{
+		width: 350px;
+		height: 450px;
+		position: absolute;
+		top: 50%;
+		bottom: 80%;
+		background-image: linear-gradient(27deg, #004E95 50%, #013A6B 50%);
+		padding: 20px;
+		
+	}
+	.centerdiv p{
+		margin-top: 20px;
+	}
+	</style>
+</html>
